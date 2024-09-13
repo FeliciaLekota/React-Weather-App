ba7a12977f0949cdf7d559a408a73ceff8a81957
FeliciaLekota <makolobe.lekota@gmail.com>
2024-09-13 11:40:07 +0200

    Initial commit

---
+++ react_weather_app/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          {" "}
+          This project was coded by{" "}
+          <a href="" target="_blank">
+            {" "}
+            Felicia Lekota
+          </a>{" "}
+          and is{" "}
+          <a href="" target="_blank">
+            Open source on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App
